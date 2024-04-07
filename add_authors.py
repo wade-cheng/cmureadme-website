09f0487aada79550bd607b298cdd43d63678fbf1
Wade <wadecheng3301@gmail.com>
2024-04-07 01:37:51 -0400

    author partial working draft

--- add_authors.py
+++ add_authors.py
@@ -1,12 +1,21 @@
+"""
+takes downloaded .tsv from google sheets
+and appends a formatted thing to `./authors.tsv`
+
+use via 
+`python3 add_authors.py ~/example/path/to/googlesheets/output.tsv`
+"""
+
+import hashlib
 import sys
 import pandas as pd
 
 NEW_AUTHORS_PATH = sys.argv[1]
 
-print(NEW_AUTHORS_PATH)
+# print(NEW_AUTHORS_PATH)
 authors_df = pd.read_csv(NEW_AUTHORS_PATH, sep="\t", header=0, dtype=str)
 cols = authors_df.columns.tolist()
-print(f"COLS: {cols}")
+# print(f"COLS: {cols}")
 #       0    1    2        3   4     5    6        7    8     9       10
 # have: time name pronouns bio major year location fact email socials pfp
 # want: id name pfp image_alt pronouns major year location fact email socials bio
@@ -25,14 +34,14 @@ new_cols = [
     cols[9],
     cols[3],
 ]
-print(f"COLS AFTER: {new_cols}")
+# print(f"COLS AFTER: {new_cols}")
 authors_df = authors_df[new_cols]
 
-print(f"new df: {authors_df}")
-print(f"double checking cols: {authors_df.columns.tolist()}")
+# print(f"new df: {authors_df}")
+# print(f"double checking cols: {authors_df.columns.tolist()}")
 
-authors_df.insert(0, "id", "TODO")
-authors_df.insert(3, "image_alt", "TODO")
+authors_df.insert(0, "id", "TEMP")
+authors_df.insert(3, "image_alt", "TEMP")
 authors_df.columns = [
     "id",
     "name",
@@ -48,5 +57,38 @@ authors_df.columns = [
     "bio",
 ]
 
-print(f"new df: {authors_df}")
-authors_df.to_csv("out.csv", sep="\t", index=False)
+def hash_string(s: str) -> str:
+    # Encode the input string to bytes
+    input_bytes = s.encode()
+
+    # Create a sha1
+    #  hash object
+    hash_object = hashlib.sha1()
+
+    # Update the hash object with the bytes to hash
+    hash_object.update(input_bytes)
+
+    # Get the hexadecimal representation of the digest
+    hex_dig = hash_object.hexdigest()
+
+    return hex_dig
+
+def get_id(row):
+    return hash_string(row["name"] + row["image"] + row["bio"])
+authors_df["id"] = authors_df.apply(get_id, axis=1)
+authors_df["image"] = "IMAGE_TEMP"
+
+def get_image_alt(row):
+    return "cover image of " + row["name"]
+authors_df["image_alt"] = authors_df.apply(get_image_alt, axis=1)
+
+
+
+# print(f"new df: {authors_df}")
+output = authors_df.to_csv(sep="\t", index=False)
+
+print("output:")
+print(output)
+
+with open("authors.tsv", "a") as f:
+    f.write(output.split("\n", 1)[1])
--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 from pathlib import Path
-import pandas as pd
+import csv
 
 # path to root dir. github pages serves website roots at /docs
 WEBSITE_ROOT = "docs"
@@ -21,22 +21,57 @@ def process(s: str, input_path: str) -> str:
     if input_path == "generator/about.html":
         author_cards: list[str] = []
 
-        authors_df = pd.read_csv("authors.tsv", sep='\t', header=0)
-        print(authors_df)
-        output = output.format(about_AUTHORS=author_cards)
+        with open("authors.tsv", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for author_row in reader:
+                author_card = ""
+
+        output = output.format(about_AUTHORS="\n".join(author_cards))
         # TODO all this
     elif input_path == "etc etc":
         output = output
-    
+
     return output
 
 
+def generate_author_profiles():
+    with open("authors.tsv", newline="") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter="\t")
+
+        for author_row in reader:
+            author_fpath = f"docs{os.sep}authors{os.sep}" + author_row["name"] + ".html"
+            author_template = f"templates{os.sep}generic_author_profile.html"
+            with (
+                open(author_fpath, "w") as output,
+                open(author_template, "r") as template,
+            ):
+                output.write(
+                    template.read().format(
+                        AuthorImage=author_row["image"],
+                        AuthorImageAltText=author_row["image_alt"],
+                        AuthorPronouns=author_row["pronouns"],
+                        AuthorMajor=author_row["major"],
+                        AuthorYear=author_row["year"],
+                        AuthorLocation=author_row["location"],
+                        AuthorFact=author_row["fact"],
+                        AuthorEmail=author_row["email"],
+                        OtherSocials=author_row["other_socials"],
+                        AuthorName=author_row["name"],
+                        AuthorBio=author_row["bio"],
+                        AuthorPrevArticles="{AuthorPrevArticles}",
+                    )
+                )
+
+
 for root, dirs, files in os.walk("generator"):
+    generate_author_profiles()
+
     for filename in files:
         # the dir fstring takes our walk and spits out which dir the current file lives in.
         # eg, a file at "generator/assets/images/img.png" turns to "{WEBSITE_ROOT}/assets/images/img.png"
         dir = f"{WEBSITE_ROOT}{os.sep}{os.sep.join(root.split(os.sep, maxsplit=1)[1:])}"
-        
+
         # create any dirs that need to be created in order to write to our file
         os.makedirs(dir, exist_ok=True)
 
